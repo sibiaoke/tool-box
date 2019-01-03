78fee1b9c7e02ae68414fcb2bd1c42eec92eb64c
vickyhoo <viczhlol@gmail.com>
2019-01-03 18:29:47 +0800

    update session key

--- packages/session/src/constents.js
+++ packages/session/src/constents.js
@@ -1,2 +1,2 @@
-export const AUTH_TOKEN_KEY = 'AUTH_TOKEN';
-export const AUTH_DATA_KEY = 'AUTH_DATA';
+export const AUTH_TOKEN_KEY = '@sibiaoke/AUTH_TOKEN';
+export const AUTH_DATA_KEY = '@sibiaoke/AUTH_DATA';
